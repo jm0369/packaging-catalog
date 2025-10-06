61d4a676f58827644a4ad0f08f5009276db3f525
Julian Mengel <info@ratisbonaflow.com>
2025-10-06 14:39:28 +0200

    api production

--- apps/api/prisma/migrations/20251005095848_add_category_fields/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `formatsAndSpecifications` on the `Category` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Category" DROP COLUMN "formatsAndSpecifications",
-ADD COLUMN     "formatsSpecifications" JSONB;
